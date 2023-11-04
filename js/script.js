6908e2d3ec9a05960aabab060a41d617aa50dba7
alazne5 <alaznegoni5@gmail.com>
2023-11-04 16:31:51 +0100

    Responsive

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const modal_container = document.getElementById('cookie-banner-container');
+const close = document.getElementById('close');
+
+
+close.addEventListener('click', () => {
+  cookie-banner-container.classList.remove('show');
+});
